a9af0b3c70d849caa4839cd7260c97e77f36af64
Yusuf Bektaş <agolho@gmail.com>
2018-01-23 16:20:44 +0300

    Eliminated the infinite Loop at the end
    replaced the nonfunctional "again" button with a working one.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var whatNow,timer,exerciseIndex;
+var whatNow,timer,seconds,exerciseIndex;
 var excersises=['Jumping Jacks','Wall Sit','Push-Up','Crunch','Step-Up','Squat','Triceps Dip','Plank','High Knees','Lunge','Push Up with Rotation','Side Plank'];
 
 $("#startButton").click(function() {
@@ -9,31 +9,33 @@ $("#startButton").click(function() {
 
 function sevenMinutes(){
     timer = setInterval(function(){ writeClock(excersises[exerciseIndex],30); }, 1000);
-    timer=35;
+    seconds=35;
 }
 
 function writeClock(whatNow,workLength){
   $('#splashScreen').addClass('jumbotron');
 
-  if (timer > 30) {
-    var countdown = timer-30;
+  if (seconds > 30) {
+    var countdown = seconds-30;
     $("#splashScreen").html('<h1>'+whatNow+'<h1> <br><h5>Get Ready!<br></h5> <h1 class="digitiser">'+countdown+'</h1>');
     countdown--;
-    timer--;
+    seconds--;
   } else {
-  $("#splashScreen").html('<h1>'+whatNow+'<h1> <br> <h1 class="digitiser">'+timer+'</h1>');
-  if (timer > 0){
-    timer--;
+  $("#splashScreen").html('<h1>'+whatNow+'<h1> <br> <h1 class="digitiser">'+seconds+'</h1>');
+  if (seconds > 0){
+    seconds--;
   }
   else {
-    timer=35;
+    seconds=35;
   if (exerciseIndex < excersises.length)
       exerciseIndex++;
     } if (exerciseIndex==excersises.length) {
       clearInterval(timer);
-      timer=0;
+      seconds=0;
       countdown=0;
-      $("#splashScreen").html('<h1>Congratulations!</h1> <br> <button class="btn btn-lg btn-warning" id="startButton" name="button">AGAIN!</button>');
+      exerciseIndex=0;
+      $("#splashScreen").html('<h3>Congratulations!</h3> <br>   <button class="btn btn-lg btn-primary" onClick="sevenMinutes()" id="startButton" name="button">AGAIN</button>');
+      return;
     }
   }
 }
